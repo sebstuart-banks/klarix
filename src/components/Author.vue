d95e8bfddb56ace7602e9b24b4857e4a12b9a2ea
Sebastian Stuart-Banks <31918514+sebstuart-banks@users.noreply.github.com>
2019-12-03 12:01:33 +0000

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -10,8 +10,7 @@
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/suits_at">Follow on Twitter</a>
-			<a href="https://github.com/suits-at/netlifycms-gridsome">GitHub</a>
+			<a href="https://linkedin.com/en/sstuartbanks">Connect on LinkedIn</a>
 		</p>
 
 	</div>
