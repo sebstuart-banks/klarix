fd58870f59e244aaccb4750297b9f3be6b250a74
sebstuart-banks <31918514+sebstuart-banks@users.noreply.github.com>
2019-12-05 12:29:34 +0000

    update about.vue

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,7 +3,6 @@
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
   </Layout>
-</template>
 
 <script>
 export default {
@@ -13,3 +12,57 @@ export default {
 }
 </script>
 
+  <Layout :show-logo="false">
+    <!-- Author intro -->
+    <Author :show-title="true" />
+
+    <!-- List posts -->
+    <div class="posts">
+      <PostCard v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node"/>
+    </div>
+
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost(filter: { published: { eq: true }}) {
+    edges {
+      node {
+        id
+        title
+        date (format: "D. MMMM YYYY")
+        timeToRead
+        description
+        cover_image (width: 770, height: 380, blur: 10)
+        ...on Post {
+        id
+        title
+        path
+        }
+        path
+        tags {
+          id
+          title
+          path
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Author from '~/components/Author.vue'
+import PostCard from '~/components/PostCard.vue'
+
+export default {
+  components: {
+    Author,
+    PostCard
+  },
+  metaInfo: {
+    title: 'Home'
+  }
+}
+</script>
